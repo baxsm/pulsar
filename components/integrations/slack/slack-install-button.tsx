822489fe5ffb6d9f8da58d6d16a3476d3175b99a
Bobby Aleem <bobbybeatmaker@hotmail.com>
2024-12-09 23:24:30 +0500

    Update slack-install-button.tsx

--- components/integrations/slack/slack-install-button.tsx
+++ components/integrations/slack/slack-install-button.tsx
@@ -8,10 +8,9 @@ const SlackInstallButton: FC = ({}) => {
   const handleClick = () => {
     const SLACK_OAUTH_URL = "https://slack.com/oauth/v2/authorize";
 
-    // const redirectUri = encodeURIComponent(
-    //   `${process.env.NEXT_PUBLIC_APP_URL}/dashboard/integrations/slack`
-    // );
-    const redirectUri = encodeURIComponent("https://a759-154-192-18-25.ngrok-free.app/dashboard/integrations/slack")
+    const redirectUri = encodeURIComponent(
+      `${process.env.NEXT_PUBLIC_APP_URL}/dashboard/integrations/slack`
+    );
 
     const clientId = String(process.env.NEXT_PUBLIC_SLACK_CLIENT_ID);
 
